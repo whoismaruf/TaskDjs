012d7863dafb529d5452a4447142bfa8a4153b60
Maruf Khan <whoismaruf@outlook.com>
2022-08-14 02:08:21 +0600

    customize admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from core.models import Task
 # Register your models here.
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'completed', 'updated_at', 'tasker')
+    list_filter = ('completed',)
+    search_fields = ('name',)
+    ordering = ('-updated_at',)
+    fields = ('name', 'description', 'completed', 'tasker','id', 'created_at', 'updated_at')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
